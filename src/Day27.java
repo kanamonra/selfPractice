9457465d57e1fd0e305458a5e149bd5eb9061f3d
kanamonra <ceceg@inha.edu>
2023-02-21 18:47:13 +0900

    self0221 other solution

--- src/Day27.java
+++ src/Day27.java
@@ -1,9 +1,6 @@
 public class Day27 {
-    public static StringBuilder solution(String str) {
-        StringBuilder rstr = new StringBuilder();
-        rstr.append(str);
-        rstr.reverse();
-        return rstr;
+    public static String solution(String str) {
+        return new StringBuilder(str).reverse()+"";
     }
     public static void main(String[] args) {
         System.out.println(solution("reverse"));
